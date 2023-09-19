5aa704514ba7456e0698cf957bbc31988ef5808c
AlanGCdB <dev.alancarlos@gmail.com>
2023-09-19 14:16:47 -0300

    home

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -3,10 +3,12 @@
     <div v-if="loading">
       <page-loading />
     </div>
-    <div v-if="api">
-      <h1>Contato</h1>
-      <p>{{ api }}</p>
-    </div>
+    <transition mode="out-in">
+      <div v-if="api">
+        <h1>{{ api.titulo }}</h1>
+        <p>{{ api }}</p>
+      </div>
+    </transition>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
+  <div class="home">
     <div v-if="loading">
       <page-loading />
     </div>
-    <div v-if="api">
-      <h1>Home</h1>
-      <p>{{ api }}</p>
-    </div>
+    <transition mode="out-in">
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{ api.titulo }}</h1>
+          <p>{{ api.descricao }}</p>
+          <router-link
+            class="btn-cursos"
+            tag="button"
+            :to="{ name: 'CursosView' }"
+            >Cursos</router-link
+          >
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{ avaliacao.nome }}</p>
+                <p>{{ avaliacao.descricao }}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img
+          src="@/assets/aprender.png"
+          alt="imgAprender"
+          srcset=""
+          width="400"
+          height="300"
+        />
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -23,3 +49,21 @@ export default {
   },
 };
 </script>
+<style scoped>
+* {
+  margin-top: 15px;
+}
+.btn-cursos {
+  border: none;
+  background: #4b8;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  padding: 15px 20px;
+  font-size: 1rem;
+  margin-top: 15px;
+  margin-bottom: 40px;
+  box-shadow: 0 4px 2px rgba(0, 0, 0, 0.1);
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
